cc20a4ddd949a30f4e24ba9fa51ba3df94d503a3
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-03-05 14:19:31 -0600

    Added Default components

---
+++ src/ui/components/MyText.java
@@ -0,0 +1,9 @@
+package ui.components;
+
+import javafx.scene.text.Text;
+
+public class MyText extends Text {
+	public MyText(String txt) {
+		super(txt);
+	}
+}
---
+++ src/ui/components/MyVBox.java
@@ -0,0 +1,9 @@
+package ui.components;
+
+import javafx.scene.layout.VBox;
+
+public class MyVBox extends VBox {
+	public MyVBox() {
+		
+	}
+}
